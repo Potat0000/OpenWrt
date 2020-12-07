28b945755de08fd735cc76a2b4f82792e67088e0
Gyj1109 <gaoyj1109@gmail.com>
2020-12-07 18:33:20 +0800

    换源

--- addon_steps.sh
+++ addon_steps.sh
@@ -12,23 +12,7 @@ cd ../../../../../..
 
 cd package/lean
 
-mkdir luci-app-openclash
-cd luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
-git branch --set-upstream-to=origin/master master
-cd ..
-
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git
-git clone https://github.com/pymumu/smartdns.git
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-
 sed -i 's/vpn/services/g' `grep 'vpn' -rl luci-app-zerotier`
 sed -i '/VPN/d' `grep 'VPN' -rl luci-app-zerotier`
 
 cd ../..
-sed -i 's/ +luci-theme-bootstrap//g' `grep '+luci-theme-bootstrap' -rl feeds/luci/collections/luci`
