c299b9294d7b33103478d32ccc31bf24964284d2
Potat0 <i@potat0.cc>
2021-08-26 18:26:43 +0800

    修改默认IP

--- addon_steps.sh
+++ addon_steps.sh
@@ -4,6 +4,8 @@ sed -i 's/Os/O3/g' include/target.mk
 sed -i 's/O2/O3/g' ./rules.mk
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
+
 mkdir -p package/base-files/files/etc/openclash/core
 cd package/base-files/files/etc/openclash/core
 curl -L https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz | tar zxf -
